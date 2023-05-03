59a1c7668400a77b51fe367ffb1a00f622c2adac
Ana Carolina <anagamaral@outlook.com>
2023-05-03 11:07:44 -0300

    Apagando arquivo de teste

--- cypress/integration/teste.spec.js
+++
@@ -1,8 +0,0 @@
-/// <reference types="Cypress" />
-
-describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
-          cy.visit('https://g1.globo.com/')
-          cy.title().should('equal', 'Central de Atendimento ao Cliente TAT')
-    })
-  })
